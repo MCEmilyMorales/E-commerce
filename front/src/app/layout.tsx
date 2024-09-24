67307f1c4c84c1691be2e95144809ac55edd0e23
Emily Morales <15ruth05@gmail.com>
2024-09-24 20:45:30 -0300

    footer and flowbite

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "../components/Navbar";
+import ComponentNavbar from "../components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar />
+        <ComponentNavbar />
         <main>{children}</main>
         <Footer />
       </body>
--- front/src/components/Footer/index.tsx
+++ front/src/components/Footer/index.tsx
@@ -1,19 +1,47 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer>
-      <div>Logotipo</div>
-      <div>
-        Contactanos
-        <Link href="#">Intagram</Link>
-        <Link href="#">Whatsapp</Link>
-      </div>
-      <div>
-        <Link href="/tabla-de-talles">Tabla de talles</Link>
-      </div>
-      <div>
-        <Link href="#">Ubicación</Link>
+      <div className="flex h-28 w-full px-10 py-5 bg-gray-300 justify-between items-center">
+        <div className="flex h-9 px-2 font-black items-center">
+          <span>Andia</span>
+        </div>
+        <div className="flex h-9 px-2 gap-4 overflow-hidden items-center">
+          Contactanos:
+          <div className="overflow-hidden">
+            <Link href="#">
+              <Image
+                src={
+                  "/images/iconfinder-social-media-applications-3instagram-4102579_113804.svg"
+                }
+                alt="Icon Instagram"
+                width={30}
+                height={30}
+                className="object-cover"
+              />
+            </Link>
+          </div>
+          <div className="overflow-hidden">
+            <Link href="#">
+              <Image
+                src={"/images/Whatsapp_icon-icons.com_66931.svg"}
+                alt="Icon Whatsapp"
+                width={30}
+                height={30}
+                className="object-cover"
+              />
+            </Link>
+          </div>
+        </div>
+        <div className="flex h-9 px-2 items-center">
+          <Link href="/tabla-de-talles">Tabla de talles</Link>
+        </div>
+        <div className="flex h-9 px-2 gap-4 items-center">
+          <Link href="#">Ubicación</Link>
+          <div className="border border-customColor-dark px-16 py-8 bg-customColor"></div>
+        </div>
       </div>
     </footer>
   );
